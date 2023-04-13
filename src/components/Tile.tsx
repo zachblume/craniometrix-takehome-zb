82d12ac24386dde9362caf21a3e1d1666db97450
zachblume <zachblume@gmail.com>
2023-04-13 15:19:31 -0400

    whose tile works now

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,12 +1,20 @@
 import React, { FC, useContext } from "react";
 
 import { MutateBoardContext, BoardContext, WhoseTurnContext } from "../App";
+import hash2DPositionTo1d from "../lib/hash2DPositionTo1d";
 
 const Tile: FC<Position> = ({ row, column }) => {
     const MutateBoard = useContext(MutateBoardContext);
     const Board = useContext(BoardContext);
     const whoseTurn = useContext(WhoseTurnContext);
-    console.log({ whoseTurn });
+
+    // Figure out who is in this tile
+    var who = "";
+    for (let [playerName, playerPositions] of Object.entries(Board)) {
+        if (playerPositions[hash2DPositionTo1d({ row, column })] === true) {
+            who = playerName;
+        }
+    }
 
     return (
         <span
@@ -20,7 +28,7 @@ const Tile: FC<Position> = ({ row, column }) => {
             }}
         >
             <span className="tile-inner">
-                ({row},{column})
+                ({row},{column},{who})
             </span>
         </span>
     );
