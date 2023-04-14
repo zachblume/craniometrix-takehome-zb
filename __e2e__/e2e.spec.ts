56fce0dbaeb21f5f5dd19b5888d53f74c6c8b5bb
zachblume <zachblume@gmail.com>
2023-04-14 16:17:28 -0400

    e2e tests working

--- __e2e__/e2e.spec.ts
+++ __e2e__/e2e.spec.ts
@@ -5,8 +5,40 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe("Clicking the board", () => {
-    test("should place a tile", async ({ page }: { page: Page }) => {
-        page.click("#board > div:nth-child(1) > div:nth-child(1)");
-        expect(await page.innerText("#board > div:nth-child(1) > div:nth-child(1)")).toBe("X");
+    test("four in a row should win for player1", async ({ page }: { page: Page }) => {
+        await page.locator("div:nth-child(5) > span:nth-child(2) > .tile").click();
+        await page.locator("div:nth-child(5) > span:nth-child(3) > .tile").click();
+        await page.locator("div:nth-child(5) > span:nth-child(2) > .tile").click();
+        await page.locator("div:nth-child(5) > span:nth-child(3) > .tile").click();
+        await page.locator("div:nth-child(4) > span:nth-child(2) > .tile").click();
+        await page.locator("div:nth-child(4) > span:nth-child(3) > .tile").click();
+        await page.locator("div:nth-child(3) > span:nth-child(2) > .tile").click();
+        await page.getByRole("heading", { name: "player1 won!" }).click();
+        // take a screenshot
+        await page.screenshot({ path: `__e2e__/screenshots/1.png` });
+        await page.getByRole("button", { name: "Restart game!" }).click();
+        //  Should return to clickable:
+        await page.locator("div:nth-child(6) > span:nth-child(7) > .tile").click();
+    });
+    test("four in a diagonal should win for player 2", async ({ page }: { page: Page }) => {
+        await page.locator("div:nth-child(6) > span:nth-child(7) > .tile").click();
+        await page.locator("div:nth-child(5) > span:nth-child(6) > .tile").click();
+        await page.locator("div:nth-child(4) > span:nth-child(5) > .tile").click();
+        await page.locator("div:nth-child(3) > span:nth-child(4) > .tile").click();
+        await page.locator("div:nth-child(2) > span:nth-child(3) > .tile").click();
+        await page.locator("span:nth-child(2) > .tile").first().click();
+        await page.locator(".tile").first().click();
+        await page.locator("div:nth-child(5) > span:nth-child(3) > .tile").click();
+        await page.locator("div:nth-child(5) > span > .tile").first().click();
+        await page.locator("div:nth-child(4) > span:nth-child(4)").click();
+        await page.locator("div:nth-child(4) > span > .tile").first().click();
+        await page.locator("div:nth-child(4) > span:nth-child(4) > .tile").click();
+        await page.locator("div:nth-child(4) > span:nth-child(5) > .tile").click();
+        await page.locator("div:nth-child(4) > span:nth-child(6) > .tile").click();
+        await page.locator("div:nth-child(4) > span:nth-child(5) > .tile").click();
+        await page.locator("div:nth-child(3) > span:nth-child(5) > .tile").click();
+        await page.getByRole("heading", { name: "player2 won!" }).click();
+        await page.getByRole("button", { name: "Restart game!" }).click();
+        await page.getByRole("heading", { name: "It'splayer1's player2's turn now!" }).click();
     });
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -15,8 +15,8 @@ export default defineConfig({
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
-    /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
+    /* Dont retry */
+    retries: 0,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -48,14 +48,14 @@ export default defineConfig({
         },
 
         /* Test against mobile viewports. */
-        // {
-        //   name: 'Mobile Chrome',
-        //   use: { ...devices['Pixel 5'] },
-        // },
-        // {
-        //   name: 'Mobile Safari',
-        //   use: { ...devices['iPhone 12'] },
-        // },
+        {
+            name: "Mobile Chrome",
+            use: { ...devices["Pixel 5"] },
+        },
+        {
+            name: "Mobile Safari",
+            use: { ...devices["iPhone 12"] },
+        },
 
         /* Test against branded browsers. */
         // {
@@ -69,9 +69,9 @@ export default defineConfig({
     ],
 
     /* Run your local dev server before starting the tests */
-    // webServer: {
-    //   command: 'npm run start',
-    //   url: 'http://127.0.0.1:3000',
-    //   reuseExistingServer: !process.env.CI,
-    // },
+    webServer: {
+        command: "npm run start",
+        url: "http://localhost:3000",
+        reuseExistingServer: !process.env.CI,
+    },
 });
